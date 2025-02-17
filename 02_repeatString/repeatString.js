1a7f733ad7e22d99f40f69b1db842d9ab3553d0d
anjamape <anjamape1972@protonmail.com>
2025-02-17 22:58:41 +0100

    Segundo ejercicio: repeatString Hecho con ayuda

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, number) {
+    if (number < 0) return "ERROR";
+    let resultWord = "";
+    for (let i = 0; i < number; i++) {
+        resultWord += word;
+    }
+    return resultWord;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
