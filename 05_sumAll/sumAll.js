dfe8a874045aa57b1bd6c5bc7f0fc2d80fed100e
anjamape <anjamape1972@protonmail.com>
2025-02-18 23:20:31 +0100

    Quinto ejercicio: sumAll Por primera vez hecho sin ayuda

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(number01, number02) {
+    let result = 0;
+    if (number01 < 0 || number02 < 0 || !Number.isInteger (number01) || !Number.isInteger (number02)) {
+        return "ERROR"
+    }
+    if (number01 <= number02) {
+        for (let i = number01; i <= number02; i++){
+            result += i
+        } return result
+    } else {
+        for (let i = number02; i <= number01; i++){
+            result += i
+        } return result
+    }
 };
 
 // Do not edit below this line
