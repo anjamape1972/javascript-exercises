683c2b8ecde1d5b236abdef7ee1b4b8c4b30a207
anjamape <anjamape1972@protonmail.com>
2025-02-18 23:54:06 +0100

    Séptimo ejercicio: tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit - 32) * (5/9);
+  return Number (tempCelsius.toFixed (1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = (tempCelsius * (9/5)) + 32;
+  return Number (tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
