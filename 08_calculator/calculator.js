18d74053f7c23c62fec15a61337d54bd1f0d0efa
anjamape <anjamape1972@protonmail.com>
2025-02-19 23:47:29 +0100

    Octavo ejercicio: calculator Sin demasiada ayuda

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function(...numbers) {
+    let result = 0;
+  for (const number of numbers) {
+    result += number;
+  }
+  return Number (result);
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+	let result = numbers [0];
+  for (let i = 1; i < numbers.length; i++) {
+    result -= numbers[i];
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) return 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array [i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	if (array.length === 0) return 0;
+  let result = array [0];
+  for (let i = 1; i < array.length; i++) {
+    result *= array [i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	if (power === 0) return 1;
+  let result = number;
+  for (let i = 1; i < power; i++) {
+    result *= number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0 || number === 1) return 1;
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
