dee6f0552a3e0e5d6b50d004f68cf4ebc2f948c3
anjamape <anjamape1972@protonmail.com>
2025-02-20 00:18:07 +0100

    Noveno ejercicio: palindromes También con algo de ayuda

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let newString = string.split("").reverse().join("");
+    if (newString === string) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
