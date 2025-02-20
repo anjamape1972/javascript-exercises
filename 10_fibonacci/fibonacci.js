2ce74d1daaff7b4a6531013df646b0f6a4fa7d7f
anjamape <anjamape1972@protonmail.com>
2025-02-20 23:06:41 +0100

    Décimo ejercicio: fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = Number (number);
+    if (number < 0) return "OOPS";
+    if (!Number.isInteger (number)) return 0;
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+    let anterior = 0;
+    let siguiente = 1;
+    for (let i = 2; i <= number; i++) {
+        let temp = anterior + siguiente;
+        anterior = siguiente;
+        siguiente = temp;
+    }
+    return siguiente;
 };
 
 // Do not edit below this line
