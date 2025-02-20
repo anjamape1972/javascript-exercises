224d5333c5f553f70f410d0252fb1f51bf406636
anjamape <anjamape1972@protonmail.com>
2025-02-20 23:15:05 +0100

    Undécimo ejercicio: getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const arrayTitles = [];
+    for (let i = 0; i < array.length; i++) {
+        arrayTitles.push (array[i].title);
+    }
+    return arrayTitles;
 };
 
 // Do not edit below this line
