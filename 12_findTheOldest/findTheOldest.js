0d87b9ca28e94fa6980bca9a52a4f8c2f42c13fe
anjamape <anjamape1972@protonmail.com>
2025-02-26 01:10:36 +0100

    Décimosegundo ejercicio: findTheOldest Hecho con mucha ayuda para luego darme cuenta del fallo tonto que era. Estaba intentando devolver una propiedad cuando el test ya la cogía solo

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    for (let i = 0; i < persons.length; i++) {
+        if (!persons[i].yearOfDeath) {
+            persons[i].yearOfDeath = new Date().getFullYear();
+        }
+        persons[i].age = persons[i].yearOfDeath - persons[i].yearOfBirth;
+    }
+    persons.sort ((a, b)=> b.age - a.age);
+    return persons[0];
 };
 
 // Do not edit below this line
